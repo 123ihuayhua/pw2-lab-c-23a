fb2d7ce6818310ac8c07a0b1aa425647561a2d7b
Paulo Andre Idalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-09 12:29:43 -0500

    se implemento el teclado relativamente ordenado

--- lab2/script.js
+++ lab2/script.js
@@ -1,1 +1,8 @@
-// aqui ira el script
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+function carga(){
+    for (var i = 0; i < numeros.length; i++) {
+        document.getElementById(i).innerHTML = numeros[i]
+    }
+}
+
+
