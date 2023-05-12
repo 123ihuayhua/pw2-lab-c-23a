c90e6e1ca840ee828e2b452fcfd500ad3b1d78bc
Paulo Hidalgo <phidalgo@unsa.edu.pe>
2023-05-12 13:12:59 +0000

    se agrego la funcion para aleatorizar

--- lab2/script.js
+++ lab2/script.js
@@ -1,4 +1,5 @@
-var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+desorden(numeros)
 function carga(){
     for (var i = 0; i < numeros.length; i++) {
         document.getElementById(i).innerHTML = numeros[i]
@@ -8,4 +9,17 @@ function teclear(i){
     var num =document.getElementById("boton"+i).innerText
     console.log(num)
 }
-
+function desorden(arreglo) {
+    var indexActual = arreglo.length;
+    var aux, random;
+    while (0 !== indexActual) {
+  
+      random = Math.floor(Math.random() * indexActual)
+      indexActual--
+  
+      aux = arreglo[indexActual]
+      arreglo[indexActual] = arreglo[random]
+      arreglo[random] = aux
+    }
+    return arreglo
+  }
