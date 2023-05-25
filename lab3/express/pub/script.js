deee0d9b8ffb8fb32d1eb0164e3576b5bdd1ef8e
Paulo Andre Idalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-25 16:04:43 -0500

    se implemento el form

---
+++ lab3/express/pub/script.js
@@ -0,0 +1,18 @@
+    function enviarDatos(){
+        const titulo = document.getElementById("title").value
+        console.log(titulo)
+    }
+    const global = "hola soy paulo"
+
+
+/*function mostrar(){
+    console.log("hola")
+    const url = 'http://localhost:3000/agenda'
+    fetch(url).then(
+        response => response.json()
+    ).then(
+        data => {
+            document.querySelector("#prueba").innerHTML = data.text
+        }
+    )
+}*/
