d3a53dd698f61f4ddfd0acd3f6ef1f509cd7cac2
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-05-28 11:23:32 -0500

    se borraron archivos y se externalizo el script

--- lab3/express/script.js
+++ lab3/express/script.js
@@ -1,4 +1,38 @@
+document.querySelector('#formCrear').onsubmit = () => {
+  const desc = document.getElementById('desc').value
+  const fecha = document.getElementById('fecha').value
+  const hora = document.getElementById('hora').value
+  agregar(desc,fecha,hora)
+  document.querySelector('#formCrear').reset();
+  return false;
+}
+function agregar(desc, fecha, hora){
+  const url = 'http://localhost:3000/eventos'
+  const data = {
+    desc: desc,
+    fecha: fecha,
+    hora: hora
+  }
+  //console.log(data)
+  const request = {
+      method: 'POST', // Podría ser GET
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  }
+    fetch(url, request)
+    .then(response => response.json())
+    .then(data2 => {
+      document.querySelector("#show").innerHTML = data2.text;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
+var i =0
 function mostrar() {
+  i=0
     const url = 'http://localhost:3000/eventos';
     fetch(url, { mode: 'cors' })
       .then(response => response.json())
@@ -9,40 +43,120 @@ function mostrar() {
           html += `<h3>${item.date}</h3>`;
           html += '<ul>';
           item.events.forEach(event => {
-              const salto = event.text.indexOf('\n')
-              //console.log(salto)
+              const salto = event.text.indexOf("\n")
+              //console.log('salto '+salto)
               var titulo
               if(salto != -1){  titulo = event.text.substring(0,salto)}
               else{ titulo = event.text}
               let descrip = event.text.substring(salto+1, event.text.length)
-              descrip = descrip.replace(/\r\n/g,"<br>")
-              const texto = event.text.replace(/\r\n/g,"<br>")
-              console.log(texto)
-
-            html += `<form id="form" action="./borrar" method="post">
+              descrip = descrip.replace(/\n/g,"<br>")
+              const texto = event.text.replace(/\n/g,"<br>")
+              //console.log('texto '+texto)
+              //console.log('descrip '+descrip)
+              i++
+            html += `<form id="formBorrar${i}">
               <input type="button" value="${titulo}" onclick="print('${descrip}')">
-              <input type="hidden"  name="date" value="${item.date}">
-              <input type="hidden" name="time" value="${event.time}">
-              <input type="button" value="editar" onclick="editarFun('${item.date}','${event.time}','${texto}')">
+              <input type="hidden"  id="date${i}" value="${item.date}">
+              <input type="hidden" id="time${i}" value="${event.time}">
+              <input type="button" value="editar" onclick="cargarEditor('${item.date}','${event.time}','${texto}')">
               <input type= "submit" value="eliminar">
           </form>`//editar
           });
           html += '</ul>';
         });
-        document.querySelector("#prueba").innerHTML = html;
+        document.querySelector("#show").innerHTML = html;
+        if(i!=0){sePuedeBorrar();}
       });
+      //console.log(i+" es el ultimo valor de i en mostrar")
   }
-  function print(texto){
-      document.querySelector("#editar").innerHTML = texto;
+  function sePuedeBorrar(){
+      for (let index = 1; index <= i; index++) {
+        let nombre = '#formBorrar'+index
+        console.log(nombre)
+        document.querySelector(nombre).onsubmit = () => {
+          eliminar(i)
+          return false;
+        }
+      }
   }
-  function editarFun(date, time,texto){
-      //console.log("entra aqui")
-      texto = texto.replace(/<br>/g,"\r\n")
-      html = `<form id="form" action="./editar" method="post">
-              <input type="hidden"  name="date" value="${date}">
-              <input type="hidden" name="time" value="${time}">
-              <textarea name="desc" cols="30" rows="10" required>${texto}</textarea><br>
-              <input type= "submit" value="confirmar" >
-          </form></li>`
-          document.querySelector("#editar").innerHTML = html;
+function print(texto){
+    document.querySelector("#editar").innerHTML = texto;
+}
+function resetear(){
+  document.querySelector("#editar").innerHTML = '';
+}
+function cargarEditor(date, time,texto){
+    //console.log("entra aqui")
+    texto = texto.replace(/<br>/g,"\r\n")
+    html = `<form id="formEditar">
+            <input type="hidden"  id="dateE" value="${date}">
+            <input type="hidden" id="timeE" value="${time}">
+            <textarea id="descE" cols="30" rows="10" required>${texto}</textarea><br>
+            <input type= "submit" value="confirmar">
+        </form>`
+        document.querySelector("#editar").innerHTML = html;
+        cargoEditor();
+        return false;
+}//editar
+function cargoEditor(){
+document.querySelector('#formEditar').onsubmit = () => {
+  console.log("entra al editor")
+  const date = document.getElementById('dateE').value
+  const time = document.getElementById('timeE').value
+  const desc2 = document.getElementById('descE').value
+  editar(date,time,desc2)
+  mostrar()
+  document.querySelector("#editar").innerHTML = '';
+  return false;
+}}//borrar
+function eliminar(i){
+  console.log('entro aqui2')
+  const date = document.getElementById('date'+i).value
+  const time = document.getElementById('time'+i).value
+  borrar(date,time)
+  //i=0
+  console.log("se borro "+i)
+  mostrar()
+  return false;
+}
+function borrar(date,time){
+  const url = 'http://localhost:3000/borrar'
+  const data = {
+    date: date,
+    time:time
   }
+  //console.log(data)
+  const request = {
+      method: 'POST', // Podría ser GET
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  }
+    fetch(url, request)
+    .then(response => response.json())
+    .then(data2 => {
+      document.querySelector("#editar").innerHTML = data2.text;
+    });
+}
+  function editar(date,time,desc){
+  const url = 'http://localhost:3000/editar'
+  const data = {
+    date: date,
+    time:time,
+    desc: desc
+  }
+  //console.log(data)
+  const request = {
+      method: 'POST', // Podría ser GET
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+  }
+    fetch(url, request)
+    .then(response => response.json())
+    .then(data2 => {
+      document.querySelector("#show").innerHTML = data2.text;
+    });
+}
