22a0caa7273ba7e61acab798d8f8d81f505d2b8e
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-04 17:32:10 -0500

    se hizo el ejercicio 2 del lab2

---
+++ lab2/latex/src/e2/script01.js
@@ -0,0 +1,6 @@
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+function carga(){
+    for (var i = 0; i < numeros.length; i++) {
+        document.getElementById(i).innerHTML = numeros[i]
+    }
+}
---
+++ lab2/latex/src/e2/script03.js
@@ -0,0 +1,28 @@
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+desorden(numeros)
+function carga(){
+    for (var i = 0; i < numeros.length; i++) {
+        document.getElementById(i).innerHTML = numeros[i]
+    }
+}
+function teclear(i){
+    var num =document.getElementById("boton"+i).innerText
+    document.getElementById("resultado").value += num
+}
+function desorden(arreglo) {
+    var indexActual = arreglo.length;
+    var aux, random;
+    while (0 !== indexActual) {
+  
+      random = Math.floor(Math.random() * indexActual)
+      indexActual--
+  
+      aux = arreglo[indexActual]
+      arreglo[indexActual] = arreglo[random]
+      arreglo[random] = aux
+    }
+    return arreglo
+  }
+  function borrar(){
+  	document.getElementById("resultado").value = ""
+  	}
