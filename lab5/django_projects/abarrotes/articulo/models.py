83854efe63c191f72a7f889a5d8961329ae6b3cb
Paulo Andre Hidalgo Chinchay <phidalgo@unsa.edu.pe>
2023-06-13 16:39:05 -0500

    se agrego el modelo Articulo

--- lab5/django_projects/abarrotes/articulo/models.py
+++ lab5/django_projects/abarrotes/articulo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    codigo = models.PositiveIntegerField()
+    codigoMarca = models.PositiveIntegerField()
+    stock = models.PositiveIntegerField()
+    precioUnitario = models.PositiveIntegerField()
+    estadoRegistro = models.TextField()
+
